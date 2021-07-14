1d169caf76d6c47cf18a9bf34af57d419075e4df
AnubisF <artem.vinogradov@hotmail.com>
2021-07-14 09:14:30 +0300

    added and change code

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,8 +1,8 @@
-import React, { useState, useEffect } from 'react';
-import { fetchDailyData } from '../../api';
-import { Line, Bar } from 'react-chartjs-2';
+import React, {useState, useEffect} from 'react';
+import {fetchDailyData} from '../../api';
+import {Line, Bar} from 'react-chartjs-2';
 
-import styles from './Chart.module.css';
+import styles from './Chart.module.css';1111111111111111111111111111111111111111111111111111111111111111111111111
 
 const Chart = () => {
     const [dailyData, setDailyData] = useState([]);
@@ -17,24 +17,24 @@ const Chart = () => {
 
     const lineChart = (
         dailyData.length
-        ? (
-        <Line 
-            data={{
-                labels: dailyData.map(({ date }) => date),
-                datasets: [{
-                    data: dailyData.map(({ confirmed }) => confirmed),
-                    label: 'Infected',
-                    borderColor: '#3333ff',
-                    fill: true,
-                }, {
-                    data: dailyData.map(({ deaths }) => deaths),
-                    label: 'deaths',
-                    borderColor: 'red',
-                    backgroundColor: 'rgba(255, 0, 0, 0.5)',
-                    fill: true,
-                }],
-            }}
-        />) : null
+            ? (
+                <Line
+                    data={{
+                        labels: dailyData.map(({date}) => date),
+                        datasets: [{
+                            data: dailyData.map(({confirmed}) => confirmed),
+                            label: 'Infected',
+                            borderColor: '#3333ff',
+                            fill: true,
+                        }, {
+                            data: dailyData.map(({deaths}) => deaths),
+                            label: 'deaths',
+                            borderColor: 'red',
+                            backgroundColor: 'rgba(255, 0, 0, 0.5)',
+                            fill: true,
+                        }],
+                    }}
+                />) : null
     );
 
     return (
@@ -42,6 +42,6 @@ const Chart = () => {
             {lineChart}
         </div>
     )
-}
+};
 
 export default Chart;
